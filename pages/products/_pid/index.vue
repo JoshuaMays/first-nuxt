cf3a5e8002035119c94f3b6dce3d223d56d03ff4
Joshua Mays <josh.mays@creditcards.com>
2018-03-10 17:50:53 -0600

    Finished up first assignment

---
+++ pages/products/_pid/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Product #{{ $route.params.pid }}</h1>
+  </div>
+</template>
+
---
+++ pages/products/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Products</h1>
+    <nuxt-link v-for="product in products" 
+      :key="product.id"
+      :to="`/products/${product.id}`"
+      tag="button">
+      Product #{{product.id}}
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          id: 1
+        },
+        {
+          id: 2
+        }
+      ]
+    };
+  }
+}
+</script>
